35f6cb941cfacc52f0e423a8ffb90fb80c894a79
yuanpu <shampool@ufl.edu>
2017-03-07 02:27:27 -0500

    smaller train config

--- tasks/Copy/train.py
+++ tasks/Copy/train.py
@@ -24,10 +24,10 @@ parser = argparse.ArgumentParser(description='PyTorch Differentiable Neural Comp
 parser.add_argument('--input_size', type=int, default= 6,
                     help='dimension of input feature')
 
-parser.add_argument('--nhid', type=int, default= 128,
+parser.add_argument('--nhid', type=int, default= 64,
                     help='humber of hidden units of the inner nn')
                     
-parser.add_argument('--nn_output', type=int, default= 128,
+parser.add_argument('--nn_output', type=int, default= 64,
                     help='humber of output units of the inner nn')
 
 parser.add_argument('--nlayer', type=int, default=2,
@@ -46,7 +46,7 @@ parser.add_argument('--mem_slot', type=int, default= 15,
 parser.add_argument('--read_heads', type=int, default=1,
                     help='number of read heads')
 
-parser.add_argument('--sequence_max_length', type=int, default= 10, metavar='N',
+parser.add_argument('--sequence_max_length', type=int, default= 4, metavar='N',
                     help='sequence_max_length')
 parser.add_argument('--cuda', action='store_true', default= True,
                     help='use CUDA')
@@ -110,7 +110,7 @@ def clip_gradient(model, clip):
 def register_nan_checks(model):
     def check_grad(module, grad_input, grad_output):
         # print(module) you can add this to see that the hook is called
-        print('hello' + type(module).__name__)
+        print(module)
         if  any(np.all(np.isnan(gi.data.numpy())) for gi in grad_input if gi is not None):
             print('NaN gradient in ' + type(module).__name__)
         
