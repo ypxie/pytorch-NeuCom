fix broadcasting for 0.1.2

neucom/memory.py
@@ -228,8 +228,9 @@ class Memory(nn.Module):
             the updated precedence vector
         """
 
-        reset_factor = 1 - reduce_sum(write_weight, 1)
-        updated_precedence_vector = reset_factor.expand_as(precedence_vector) * precedence_vector + write_weight
+        reset_factor = 1 - reduce_sum(write_weight, 1, keep_dim=True)
+        updated_precedence_vector = reset_factor.expand_as(precedence_vector) * \
+                                    precedence_vector + write_weight
         #apply_dict(locals())                                                
         return updated_precedence_vector
     

neucom/utils.py
@@ -151,10 +151,10 @@ def cosine_distance(memory_matrix, cos_keys):
     Returns: Tensor (batch_size, mem_slot, number_of_keys)
         The list of lookup weightings for each provided key
     """
-    memory_norm = torch.norm(memory_matrix, 2, 2)
-    keys_norm = torch.norm(cos_keys, 2, 1)
+    memory_norm = torch.norm(memory_matrix, 2, 2, keepdim=True)
+    keys_norm = torch.norm(cos_keys, 2, 1,keepdim=True)
     
-    normalized_mem = torch.div(memory_matrix, memory_norm.expand_as(memory_matrix) + 1e-8)
+    normalized_mem  = torch.div(memory_matrix, memory_norm.expand_as(memory_matrix) + 1e-8)
     normalized_keys = torch.div(cos_keys, keys_norm.expand_as(cos_keys) + 1e-8)
     
     out =  torch.bmm(normalized_mem, normalized_keys)